8b4d2e3eee78f8662711b8729ac1dc17cb092bdf
Lance Onnagan <lronnagan@student.apc.edu.ph>
2025-01-27 18:35:53 +0800

    add 2nd fruit update

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -17,11 +17,11 @@
   </script>
   
   <style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: green;
-  }
-</style>
+    #app > div {
+      border: dashed black 1px;
+      display: inline-block;
+      margin: 10px;
+      padding: 10px;
+      background-color: yellow;
+    }
+  </style>
