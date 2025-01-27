f9defc46b0343eaf090e82be8da03e2c5b363aab
Lance Onnagan <lronnagan@student.apc.edu.ph>
2025-01-27 18:40:15 +0800

    update 2nd fruit

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -1,27 +1,27 @@
 <template>
-    <div>
-      <h2>{{ name }}</h2>
-      <p>{{ message }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        name: 'Mangoes',
-        message: 'I like mangoes'
-      }
+  <div  class="mango">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Mangoes',
+      message: 'I like Mangoes'
     }
-  };
-  </script>
-  
-  <style>
-    #app > div {
-      border: dashed black 1px;
-      display: inline-block;
-      margin: 10px;
-      padding: 10px;
-      background-color: yellow;
-    }
-  </style>
+  }
+};
+</script>
+
+<style>
+  .mango {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightyellow;
+}
+</style>
