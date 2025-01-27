56e89feb12f4e971a302aa971c6f01e82eb30f91
Lance Onnagan <lronnagan@student.apc.edu.ph>
2025-01-27 18:33:00 +0800

    add 2nd fruit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <food-item/>
+  <food-item/> <br>
+  <food-item2/> <br>
   <food-item/>
 </template>
 
 <script></script>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-</style>
+<style></style>
