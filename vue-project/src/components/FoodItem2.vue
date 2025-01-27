1509059a9d0df758c9631f51f21da5ae7508762b
Lance Onnagan <lronnagan@student.apc.edu.ph>
2025-01-27 18:42:11 +0800

    r

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -1,27 +1,27 @@
 <template>
-  <div  class="mango">
-    <h2>{{ name }}</h2>
-    <p>{{ message }}</p>
-  </div>
-</template>
+    <div  class="mango">
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
 
-<script>
-export default {
-  data() {
-    return {
-      name: 'Mangoes',
-      message: 'I like Mangoes'
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Mangoes',
+        message: 'I like Mangoes'
+      }
     }
-  }
-};
-</script>
+  };
+  </script>
 
-<style>
-  .mango {
-  border: dashed black 1px;
-  display: inline-block;
-  margin: 10px;
-  padding: 10px;
-  background-color: yellow;
-}
-</style>
+  <style>
+    .mango {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: yellow;
+  }
+  </style>
